9954593753e74bcc7a4f5f4931ee1a84d1e9b037
Lais Aguiar <lais.aguiar.info@gmail.com>
2018-04-27 20:08:01 -0300

    Snippetfy curso GoNode

---
+++ app/middlewares/auth.js
@@ -0,0 +1,7 @@
+module.exports = (req, res, next) => {
+  if (req.session && req.session.user) {
+    return next();
+  }
+  req.flash('error', 'Não autorizado');
+  return res.redirect('/');
+};
---
+++ config/database.js
@@ -0,0 +1,8 @@
+module.exports = {
+  username: 'root',
+  password: 'admin',
+  database: 'snippetfy',
+  host: '127.0.0.1',
+  dialect: 'mysql',
+
+};
